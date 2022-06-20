5e7404c1e9352c2a259195f6170f082f9df44338
narendra <narendrakota150@gmail.com>
2022-06-20 19:37:10 +0530

    reactat

---
+++ present/.next/static/N-SwNAPoJCHu6U6MW0CrB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/2c796e83-0fee13a8cdf60dad.js","static/chunks/483-f4067be2857fe158.js","static/chunks/pages/index-0feebda57fec1dd4.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
